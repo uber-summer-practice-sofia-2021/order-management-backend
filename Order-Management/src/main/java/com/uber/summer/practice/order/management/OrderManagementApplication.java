bc799fdd349d0f6ffaa4e308decb38a525084984
lpstamenov <lpstamenov@gmail.com>
2021-09-08 11:47:19 +0300

    Write in database

--- Order-Management/src/main/java/com/uber/summer/practice/order/management/OrderManagementApplication.java
+++ Order-Management/src/main/java/com/uber/summer/practice/order/management/OrderManagementApplication.java
@@ -1,5 +1,7 @@
 package com.uber.summer.practice.order.management;
 
+import com.uber.summer.practice.order.management.dao.OrdersDAO;
+import com.uber.summer.practice.order.management.order.ClientOrder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderManagementApplication {
 
     public static void main(String[] args) {
+        //OrdersDAO dao = new OrdersDAO();
+        //dao.update(new ClientOrder());
         SpringApplication.run(OrderManagementApplication.class, args);
     }
 
--- Order-Management/src/main/java/com/uber/summer/practice/order/management/dao/OrdersDAO.java
+++ Order-Management/src/main/java/com/uber/summer/practice/order/management/dao/OrdersDAO.java
@@ -1,12 +1,16 @@
 package com.uber.summer.practice.order.management.dao;
 
 import com.uber.summer.practice.order.management.order.ClientOrder;
+import com.uber.summer.practice.order.management.order.address.Address;
 import com.uber.summer.practice.order.management.order.status.Status;
 import com.uber.summer.practice.order.management.order.status.state.State;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 import java.util.UUID;
 import java.util.function.Predicate;
@@ -46,5 +50,22 @@ public class OrdersDAO {
     //put object
     public void update(ClientOrder order) {
         //update the whole row
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("stud");
+        EntityManager em = emf.createEntityManager();
+        //em.getTransaction().begin();
+        //List<ClientOrder> result = em.createQuery( "FROM client_order", ClientOrder.class ).getResultList();
+
+
+        em.getTransaction().begin();
+
+        ClientOrder obj = new ClientOrder();
+
+        obj.setClientName("Harry Potter");
+
+        em.persist(obj);
+
+        em.getTransaction().commit();
+
+        em.clear();
     }
 }
