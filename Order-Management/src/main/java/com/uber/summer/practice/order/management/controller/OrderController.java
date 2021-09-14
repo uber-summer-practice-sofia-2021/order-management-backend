dc3b1f818ffb00047796a985761767447f5454e1
Radina <r_nuneva@abv.bg>
2021-09-14 15:08:20 +0300

    Change HibernateException to Throwable

--- Order-Management/src/main/java/com/uber/summer/practice/order/management/controller/OrderController.java
+++ Order-Management/src/main/java/com/uber/summer/practice/order/management/controller/OrderController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-//import org.sqlite.SQLiteException;
 
 import java.util.List;
 import java.util.Map;
@@ -36,24 +35,23 @@ public class OrderController {
             @RequestParam(defaultValue = "1") int size
     ) {
         try {
-//            System.out.println(qp);
             System.out.println(tags);
             return orderService.getOrders(Optional.ofNullable(max_weight),
                                           Optional.ofNullable(max_height),
                                           Optional.ofNullable(max_length),
                                           Optional.ofNullable(max_width),
                                           Optional.ofNullable(tags),page,size);
-        } catch(HibernateException e) {
+        } catch(Throwable e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to fulfil the request", e);
         }
     }
 
 
     @GetMapping("/orders/{id}")
-    public ClientOrder getOrderByID(@PathVariable UUID id) { //add exception handling if "id" is missing
+    public ClientOrder getOrderByID(@PathVariable UUID id) { 
         try {
             return orderService.getOrderByID(id);
-        } catch (HibernateException e) {
+        } catch (Throwable e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to fulfil the request", e);
         }
     }
@@ -62,7 +60,7 @@ public class OrderController {
     public ResponseEntity<Map<String, Object>> addOrder(@RequestBody ClientOrder order) {
         try {
             return orderService.addOrder(order);
-        } catch (HibernateException e) {
+        } catch (Throwable e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to fulfil the request", e);
         }
     }
@@ -71,7 +69,7 @@ public class OrderController {
     public ResponseEntity<Map<String, Object>> updateState(@PathVariable UUID id, @PathVariable Status status) {
         try {
             return orderService.updateOrderState(id, status);
-        } catch (HibernateException e) {
+        } catch (Throwable e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to fulfil the request", e);
         }
     }
